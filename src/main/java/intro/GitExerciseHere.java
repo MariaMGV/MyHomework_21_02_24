9c878b3527f62eab15a540e8e383eed019ec9994
Maria Vasileva <mimutu.vasileva@gmail.com>
2024-11-22 11:21:26 +0200

    new method 'exerciseForSecondMasterFeature' is added + new file 'GitMergeSecondMasterFeature' is added

--- src/main/java/intro/GitExerciseHere.java
+++ src/main/java/intro/GitExerciseHere.java
@@ -3,8 +3,12 @@ package intro;
 public class GitExerciseHere {
     public static void main(String[] args) {
         printMyName();
+        exerciseForSecondMasterFeature();
     }
     public static void printMyName(){
         System.out.println("Maria Vasileva");
+    }
+    public static void exerciseForSecondMasterFeature(){
+        System.out.println("New method for Second-master-feature's exercise");
     }
 }
---
+++ src/main/java/intro/GitMergeSecondMasterFeature.java
@@ -0,0 +1,7 @@
+package intro;
+
+public class GitMergeSecondMasterFeature {
+    public static void main(String[] args) {
+        System.out.println("Second-master-feature is merged");
+    }
+}
