0265e1bb599d5a33bb686353feedcd3f3d709ad2
Maria Vasileva <mimutu.vasileva@gmail.com>
2024-11-27 12:19:46 +0200

    New files added

---
+++ src/main/java/intro/SecondGitExercise.java
@@ -0,0 +1,13 @@
+package intro;
+
+public class SecondGitExercise {
+
+    public static void main(String[] args) {
+        printMyMessage();
+    }
+
+    public static void printMyMessage(){
+        System.out.println("GIT exercises continue");
+    }
+
+}
---
+++ src/main/java/intro/ThirdGitExercise.java
@@ -0,0 +1,7 @@
+package intro;
+
+public class ThirdGitExercise {
+    public static void main(String[] args) {
+        System.out.println("Message here");
+    }
+}
